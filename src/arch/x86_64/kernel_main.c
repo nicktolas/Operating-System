c94eb701e7d63a5af6c6be0a3563259d9e728a1f
nicktolas <ntolas20@gmail.com>
2022-04-06 14:20:44 -0700

    display splash before load

--- src/arch/x86_64/kernel_main.c
+++ src/arch/x86_64/kernel_main.c
@@ -5,13 +5,15 @@ void kmain(void)
 {
     int hold;
 
+    VGA_clear();
+    VGA_background();
     // infinite loop for debugging purposes
     hold = 1;
     while(hold){;}
 
     VGA_clear();
     VGA_background();
-    // VGA_display_str("YEET");
+    VGA_display_str("YEET");
 
     while(1)
     {
