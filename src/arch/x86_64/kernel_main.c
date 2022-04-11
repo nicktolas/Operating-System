3c34c94b9db3e7fe5dd31a39b0030163ecc57d09
nicktolas <ntolas20@gmail.com>
2022-04-10 23:21:40 -0700

    Forgot to swap negatives for unsigned

--- src/arch/x86_64/kernel_main.c
+++ src/arch/x86_64/kernel_main.c
@@ -15,10 +15,10 @@ void kmain(void)
     VGA_clear();
 
     printk("Printk Testing\r\n");
-    printk("Short 420\r\n\tshort: %d\r\n\tunsigned short: %u\r\n\thex: %x\r\n", 420, -420, 420);
-    printk("Int 42069\r\n\tint: %d\r\n\tunsigned int: %u\r\n\thex: %x\r\n", 42069, -42069, 42069);
-    printk("Long 42069x3\r\n\tlong int: %ld\r\n\tunsigned long int: %lu\r\n\tlong hex: %lx\r\n", 420694206942069, -420694206942069, 420694206942069);
-    printk("Long Long 42069x4\r\n\tll int: %qd\r\n\tunsigned ll int: %qu\r\n\tll hex: %qx\r\n", big_num, neg_big_num, big_num);
+    printk("Short 420\r\n\tshort: %d\r\n\tunsigned short: %u\r\n\thex: %x\r\n", -420, 420, 420);
+    printk("Int 42069\r\n\tint: %d\r\n\tunsigned int: %u\r\n\thex: %x\r\n", -42069, 42069, 42069);
+    printk("Long 42069x3\r\n\tlong int: %ld\r\n\tunsigned long int: %lu\r\n\tlong hex: %lx\r\n", -420694206942069, 420694206942069, 420694206942069);
+    printk("Long Long 42069x4\r\n\tll int: %qd\r\n\tunsigned ll int: %qu\r\n\tll hex: %qx\r\n", neg_big_num, big_num, big_num);
     printk("String Testing\r\n\tUnsigned char: %c\r\n\tpointer %p\r\n\tstring:%s\r\n", -1, &hold, "Yeet");
 
     
